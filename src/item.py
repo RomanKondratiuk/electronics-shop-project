28aae7dea3081d939283096a0519be522112d113
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-05-25 22:09:03 -0400

    add changes

--- src/item.py
+++ src/item.py
@@ -1,4 +1,8 @@
-from typing import Callable, Iterable
+
+
+import csv
+
+from poetry.console.commands import self
 
 
 class Item:
@@ -17,9 +21,20 @@ class Item:
         :param quantity: Количество товара в магазине.
         """
         self.all.append(self)
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name: str):
+        if len(name) <= 10:
+            self.__name = name
+        else:
+             print("Длина наименования товара превышает 10 символов.")
 
 
     def calculate_total_price(self):
@@ -30,11 +45,35 @@ class Item:
           """
         return self.price * self.quantity
 
-
-
-
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+         класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv
+        #  """
+        cls.all.clear()
+        with open("/Users/romankondratiuk/Desktop/electronics-shop-project/src/items.csv", newline="") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls.all.append(row)
+            return f' в файле {cls.all} записей с данными по товарам'
+
+
+                # cls.all.append(row)
+
+           # print(f"в файле {len(cls.all)} записей с данными по товарам")
+
+
+    @staticmethod
+    def string_to_number(number):
+        """
+        статический метод, возвращающий число из числа-строки
+        """
+        return int(float(number))
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -14,8 +14,6 @@ def test_calculate_total_price_1(item1):
     assert item1.calculate_total_price() == 200000
 
 
-
-
 @pytest.fixture
 def item2():
     return Item("Ноутбук", 20000, 5)
@@ -27,8 +25,30 @@ def test_calculate_total_price_2(item2):
 
 
 def test_apply_discount():
-
     item1 = Item("Смартфон", 10000, 20)
     Item.pay_rate = 0.8
     item1.apply_discount()
     assert item1.price == 8000.0
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+def test_instantiate_from_csv():
+    a = Item.name = 'Смартфон'
+    b = Item.name = 'Ноутбук'
+    c = Item.name = 'Кабель'
+    d = Item.name = 'Мышка'
+    e = Item.name = 'Клавиатура'
+
+    assert a == 'Смартфон'
+    assert b == 'Ноутбук'
+    assert c == 'Кабель'
+    assert d == 'Мышка'
+    assert e == 'Клавиатура'
+
+
+
